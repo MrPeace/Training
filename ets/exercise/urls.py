22ce3d623860479a7ff31650ddc571745bb312cc
Pete Ostergren <ostergren.pete@gmail.com>
2022-09-19 17:08:37 -0400

    CRUD Create

--- ets/exercise/urls.py
+++ ets/exercise/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.diary_list, name='diary_list'),
+    path('diary/create/', views.diary_create, name='diary_create'),
+    path('diary/create/diary_create_record/', views.diary_create_record, name='diary_create_record')
     ]
--- ets/exercise/views.py
+++ ets/exercise/views.py
@@ -1,11 +1,27 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from django.template import loader
 
 from .models import Diary, Exercise, Workout
 
 
-def index(request):
+def diary_list(request):
     d = Diary.objects.all()
-    template = loader.get_template('index.html')
-    context = {'diary':d}
+    template = loader.get_template('diary_list.html')
+    context = {'diary': d}
     return HttpResponse(template.render(context, request))
+
+
+def diary_create(request):
+    template = loader.get_template('diary_create.html')
+    return HttpResponse(template.render({}, request))
+
+
+def diary_create_record(request):
+    x = request.POST['diary_date']
+    w = request.POST['diary_weight']
+    y = request.POST['diary_duration']
+    z = request.POST['diary_activity']
+    diary = Diary(diary_date=x, diary_weight=w, diary_duration=y, diary_activity=z)
+    diary.save()
+    return HttpResponseRedirect(reverse('diary_list'))
