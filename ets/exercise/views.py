89ce8614bd035bed3fe1fcfd9402f9b0372eddce
Pete Ostergren <ostergren.pete@gmail.com>
2022-09-19 15:17:52 -0400

    CRUD Retrieve

--- ets/exercise/views.py
+++ ets/exercise/views.py
@@ -1,7 +1,11 @@
 from django.http import HttpResponse
 from django.template import loader
 
+from .models import Diary, Exercise, Workout
+
 
 def index(request):
+    d = Diary.objects.all()
     template = loader.get_template('index.html')
-    return HttpResponse(template.render())
+    context = {'diary':d}
+    return HttpResponse(template.render(context, request))
