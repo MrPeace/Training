e6ae10cf71ad92088c81e5cdf781492457c9b4da
Pete Ostergren <ostergren.pete@gmail.com>
2022-09-19 13:38:43 -0400

    Running app with hard-coded template

--- ets/exercise/views.py
+++ ets/exercise/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
+from django.template import loader
 
 
 def index(request):
-    return HttpResponse('Me')
-
-
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render())
