239a6bd8ca938a09adb03fdea6db2a6cd96194bc
Pete Ostergren <ostergren.pete@gmail.com>
2022-09-19 14:42:52 -0400

    Added models

--- ets/exercise/models.py
+++ ets/exercise/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Diary(models.Model):
+    diary_date = models.DateField(default='django.utils.timezone.now')
+    diary_weight = models.DecimalField(max_digits=4, decimal_places=1)
+    diary_duration = models.IntegerField(default=0)
+    diary_activity = models.CharField(max_length=20)
+
+
+class Exercise(models.Model):
+    exercise_code = models.CharField(max_length=6)
+    exercise_name = models.CharField(max_length=20)
+    exercise_steps = models.TextField()
+
+
+class Workout(models.Model):
+    weight = models.IntegerField(default=0)
+    sets = models.IntegerField(default=0)
+    reps = models.IntegerField(default=0)
+    diary = models.ForeignKey(Diary, on_delete=models.CASCADE)
+    exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
