ee52fe303788cac953e44f178af8768391ba8c5b
Pete Ostergren <ostergren.pete@gmail.com>
2022-10-03 12:03:03 -0400

    Dashboard converted

--- ets/exercise/urls.py
+++ ets/exercise/urls.py
@@ -2,10 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.diary_list, name='diary_list'),
-    path('diary/create/', views.diary_create, name='diary_create'),
-    path('diary/create/diary_create_record/', views.diary_create_record, name='diary_create_record'),
-    path('diary/create/diary_delete_record/<int:id>', views.dairy_delete_record, name='diary_delete_record'),
-    path('diary/create/diary_update/<int:id>', views.diary_update, name='diary_update'),
-    path('diary/create/diary_update/diary_update_record/<int:id>', views.diary_create_record, name='diary_update_record'),
-    ]
+    path('', views.dashboard, name='dashboard'),
+    path('diary/', views.diary, name='diary'),
+    path('exercise/', views.exercise, name='exercise'),
+]
--- ets/exercise/views.py
+++ ets/exercise/views.py
@@ -1,55 +1,16 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+from django.shortcuts import render
 from django.template import loader
 
-from .models import Diary, Exercise, Workout
+
+def dashboard(request):
+    return render(request, 'exercise/dashboard.html')
 
 
-def diary_list(request):
-    d = Diary.objects.all()
-    template = loader.get_template('diary_list.html')
-    context = {'diary': d}
-    return HttpResponse(template.render(context, request))
+def diary(request):
+    return render(request, 'exercise/diary.html')
 
 
-def diary_create(request):
-    template = loader.get_template('diary_create.html')
-    return HttpResponse(template.render({}, request))
-
-
-def diary_create_record(request):
-    x = request.POST['diary_date']
-    w = request.POST['diary_weight']
-    y = request.POST['diary_duration']
-    z = request.POST['diary_activity']
-    diary = Diary(diary_date=x, diary_weight=w, diary_duration=y, diary_activity=z)
-    diary.save()
-    return HttpResponseRedirect(reverse('diary_list'))
-
-
-def dairy_delete_record(request, id):
-    d = Diary.objects.get(id=id)
-    d.delete()
-    return HttpResponseRedirect(reverse('diary_list'))
-
-
-def diary_update(request, id):
-    d = Diary.objects.get(id=id)
-    template = loader.get_template('diary_update.html')
-    context = {'diary': d}
-    return HttpResponse(template.render(context, request))
-
-
-def diary_update_record(request, row):
-    diary_date = request.POST['diary_date']
-    diary_weight = request.POST['diary_weight']
-    diary_duration = request.POST['diary_duration']
-    diary_activity = request.POST['diary_activity']
-
-    diary = Diary.objects.get(id=row)
-    diary.diary_date = diary_date
-    diary.diary_weight = diary_weight
-    diary.diary_duration = diary_duration
-    diary.diary_activity = diary_activity
-    diary.save()
-    return HttpResponseRedirect(reverse('diary_list'))
+def exercise(request):
+    return render(request, 'exercise/exercise.html')
