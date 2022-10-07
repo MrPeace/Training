945248ef97d527a55a2ffab178da2723a5f0db26
Pete Ostergren <ostergren.pete@gmail.com>
2022-10-06 20:02:46 -0400

    add static files

--- ets/ets/settings.py
+++ ets/ets/settings.py
@@ -3,6 +3,8 @@ from dotenv import load_dotenv
 from pathlib import Path
 from os import getenv
 
+import os
+
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -110,7 +112,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+MEDIA_URL = '/images/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
